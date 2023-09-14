cdf0656fc08030465052933960651721824d13f8
felipe-py <112325208+felipe-py@users.noreply.github.com>
2023-09-14 20:51:45 -0300

    :sparkles: feat: Implementação do leitor DAO

--- src/main/java/com/uefs/gerenciadorparabibliotecas/dao/leitor/LeitorDAO.java
+++ src/main/java/com/uefs/gerenciadorparabibliotecas/dao/leitor/LeitorDAO.java
@@ -3,5 +3,8 @@ package com.uefs.gerenciadorparabibliotecas.dao.leitor;
 import com.uefs.gerenciadorparabibliotecas.dao.CRUD;
 import com.uefs.gerenciadorparabibliotecas.model.Leitor;
 
+import java.util.List;
+
 public interface LeitorDAO extends CRUD<Leitor> {
+    List<Leitor> getLeitores();
 }
--- src/main/java/com/uefs/gerenciadorparabibliotecas/dao/leitor/LeitorDAOList.java
+++ src/main/java/com/uefs/gerenciadorparabibliotecas/dao/leitor/LeitorDAOList.java
@@ -2,20 +2,41 @@ package com.uefs.gerenciadorparabibliotecas.dao.leitor;
 
 import com.uefs.gerenciadorparabibliotecas.model.Leitor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LeitorDAOList implements LeitorDAO {
-    @Override
-    public Leitor criar(Leitor obj) {
-        return null;
+
+    private List<Leitor> leitores = new ArrayList<>();
+
+    public LeitorDAOList() {
+        this.leitores = leitores;
+    }
+
+    public List<Leitor> getLeitores() {
+        return leitores;
     }
 
     @Override
-    public void deletar(int id) {
+    public Leitor criar(Leitor leitor) {
+        this.leitores.add(leitor);
+        return leitor;
+    }
 
+
+
+    @Override
+    public void deletar(int id) {
+        for(Leitor leitor: this.leitores){
+            if(leitor.getUserID() == id){
+                this.leitores.remove(leitor);
+            }
+        }
     }
 
     @Override
     public void resetar() {
-
+        this.leitores = new ArrayList<>();
     }
 
     @Override
@@ -23,10 +44,13 @@ public class LeitorDAOList implements LeitorDAO {
         return null;
     }
 
-
-
     @Override
     public Leitor procurarPorID(int id) {
+        for (Leitor leitor : this.leitores) {
+            if (leitor.getUserID() == id) {
+                return leitor;
+            }
+        }
         return null;
     }
 }
