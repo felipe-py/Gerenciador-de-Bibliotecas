26dd9f53ce2fe6fb21e1a185132e5a86e2deff7c
felipe-py <112325208+felipe-py@users.noreply.github.com>
2023-09-14 22:08:54 -0300

    🧪 tests: Implementação dos testes para o empréstimo DAO

---
+++ src/test/java/com/uefs/gerenciadorparabibliotecas/dao/emprestimo/EmprestimoDAOListTest.java
@@ -0,0 +1,70 @@
+package com.uefs.gerenciadorparabibliotecas.dao.emprestimo;
+
+import com.uefs.gerenciadorparabibliotecas.dao.MasterDAO;
+import com.uefs.gerenciadorparabibliotecas.model.CategoriaLivro;
+import com.uefs.gerenciadorparabibliotecas.model.Emprestimo;
+import com.uefs.gerenciadorparabibliotecas.model.Leitor;
+import com.uefs.gerenciadorparabibliotecas.model.Livro;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmprestimoDAOListTest {
+
+    private Emprestimo emprestimo;
+    private Livro livro;
+    private Leitor leitor;
+    @BeforeEach
+    void beforeAll() {
+        LocalDate data = LocalDate.of(1998, 1, 1);
+        LocalDate dataEmprestimo = LocalDate.of(2023,9,14);
+        LocalDate dataDevolucao = LocalDate.of(2023,9,21);
+        this.livro = new Livro("Diário de um banana","Zezinho","Cultura","4455883",data,
+                CategoriaLivro.OUTRA,"ala c");
+        this.leitor = new Leitor("Lucas","Feira VI","senha123","40028922",
+                4477);
+        this.emprestimo = new Emprestimo(dataEmprestimo,dataDevolucao,this.livro,this.leitor);
+        MasterDAO.getEmprestimoDAO().criar(emprestimo);
+    }
+
+    @AfterEach
+    void tearDown() {
+        MasterDAO.getEmprestimoDAO().resetar();
+    }
+
+    @Test
+    void criar() {
+        LocalDate dataEmprestimoAux = LocalDate.of(2023,9,14);
+        LocalDate dataDevolucaoAux = LocalDate.of(2023,9,21);
+        assertEquals(dataEmprestimoAux, MasterDAO.getEmprestimoDAO().procurarPorID(this.emprestimo.getEmprestimoID()).getDataEmprestimo());
+        assertEquals(dataDevolucaoAux, MasterDAO.getEmprestimoDAO().procurarPorID(this.emprestimo.getEmprestimoID()).getDataDevolucao());
+        assertEquals(this.livro, MasterDAO.getEmprestimoDAO().procurarPorID(this.emprestimo.getEmprestimoID()).getLivroEmprestado());
+        assertEquals(this.leitor, MasterDAO.getEmprestimoDAO().procurarPorID(this.emprestimo.getEmprestimoID()).getMutuario());
+    }
+
+    @Test
+    void deletar() {
+        LocalDate dataEmprestimo = LocalDate.of(2023,9,14);
+        LocalDate dataDevolucao = LocalDate.of(2023,9,21);
+        Emprestimo emprestimoAux = new Emprestimo(dataEmprestimo,dataDevolucao,this.livro,this.leitor);
+        MasterDAO.getEmprestimoDAO().criar(emprestimoAux);
+        MasterDAO.getEmprestimoDAO().deletar(this.emprestimo.getEmprestimoID());
+        assertNull(MasterDAO.getEmprestimoDAO().procurarPorID(this.emprestimo.getEmprestimoID()));
+        assertNotNull(MasterDAO.getEmprestimoDAO().procurarPorID(emprestimoAux.getEmprestimoID()));
+    }
+
+    @Test
+    void resetar() {
+        MasterDAO.getEmprestimoDAO().resetar();
+        assertNull(MasterDAO.getEmprestimoDAO().procurarPorID(this.emprestimo.getEmprestimoID()));
+    }
+
+    @Test
+    void procurarPorID() {
+        assertEquals(emprestimo, MasterDAO.getEmprestimoDAO().procurarPorID(0));
+    }
+}
