f073223f641895c03dedc755bbbcc72224731298
felipe-py <112325208+felipe-py@users.noreply.github.com>
2023-09-15 16:49:37 -0300

    :sparkles: feat: Implementação DAO funcionário

--- src/main/java/com/uefs/gerenciadorparabibliotecas/dao/funcionario/FuncionarioDAO.java
+++ src/main/java/com/uefs/gerenciadorparabibliotecas/dao/funcionario/FuncionarioDAO.java
@@ -3,5 +3,8 @@ package com.uefs.gerenciadorparabibliotecas.dao.funcionario;
 import com.uefs.gerenciadorparabibliotecas.dao.CRUD;
 import com.uefs.gerenciadorparabibliotecas.model.Funcionario;
 
+import java.util.List;
+
 public interface FuncionarioDAO extends CRUD<Funcionario> {
+    List<Funcionario> getFuncionarios();
 }
--- src/main/java/com/uefs/gerenciadorparabibliotecas/dao/funcionario/FuncionarioDAOList.java
+++ src/main/java/com/uefs/gerenciadorparabibliotecas/dao/funcionario/FuncionarioDAOList.java
@@ -1,21 +1,43 @@
 package com.uefs.gerenciadorparabibliotecas.dao.funcionario;
 
+import com.uefs.gerenciadorparabibliotecas.model.Administrador;
+import com.uefs.gerenciadorparabibliotecas.model.Bibliotecario;
 import com.uefs.gerenciadorparabibliotecas.model.Funcionario;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FuncionarioDAOList implements FuncionarioDAO{
+
+    private List<Funcionario> funcionarios = new ArrayList<>();
+
+    public FuncionarioDAOList() {
+        this.funcionarios = funcionarios;
+    }
+
     @Override
-    public Funcionario criar(Funcionario obj) {
-        return null;
+    public Funcionario criar(Funcionario funcionario) {
+        if (funcionario instanceof Bibliotecario) {
+            Bibliotecario bibliotecario = (Bibliotecario) funcionario;
+        } else if (funcionario instanceof Administrador) {
+            Administrador administrador = (Administrador) funcionario;
+        }
+        this.funcionarios.add(funcionario);
+        return funcionario;
     }
 
     @Override
     public void deletar(int id) {
-
+        for(Funcionario funcionario: this.funcionarios){
+            if(funcionario.getUserID() == id){
+                funcionarios.remove(funcionario);
+            }
+        }
     }
 
     @Override
     public void resetar() {
-
+        this.funcionarios = new ArrayList<>();
     }
 
     @Override
@@ -25,6 +47,16 @@ public class FuncionarioDAOList implements FuncionarioDAO{
 
     @Override
     public Funcionario procurarPorID(int id) {
+        for(Funcionario funcionario: this.funcionarios){
+            if(funcionario.getUserID() == id){
+                return funcionario;
+            }
+        }
         return null;
+    }
+
+    @Override
+    public List<Funcionario> getFuncionarios() {
+        return this.funcionarios;
     }
 }
