1f60c8d26a214777f3fe10893aa7af2fbb4db99c
felipe-py <112325208+felipe-py@users.noreply.github.com>
2023-09-14 22:07:31 -0300

    :sparkles: feat: Implementação do empréstimo DAOI

--- src/main/java/com/uefs/gerenciadorparabibliotecas/dao/emprestimo/EmprestimoDAO.java
+++ src/main/java/com/uefs/gerenciadorparabibliotecas/dao/emprestimo/EmprestimoDAO.java
@@ -3,6 +3,8 @@ package com.uefs.gerenciadorparabibliotecas.dao.emprestimo;
 import com.uefs.gerenciadorparabibliotecas.dao.CRUD;
 import com.uefs.gerenciadorparabibliotecas.model.Emprestimo;
 
-public interface EmprestimoDAO extends CRUD<Emprestimo> {
+import java.util.List;
 
+public interface EmprestimoDAO extends CRUD<Emprestimo> {
+    List<Emprestimo> getEmprestimos();
 }
--- src/main/java/com/uefs/gerenciadorparabibliotecas/dao/emprestimo/EmprestimoDAOList.java
+++ src/main/java/com/uefs/gerenciadorparabibliotecas/dao/emprestimo/EmprestimoDAOList.java
@@ -2,19 +2,44 @@ package com.uefs.gerenciadorparabibliotecas.dao.emprestimo;
 
 import com.uefs.gerenciadorparabibliotecas.model.Emprestimo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmprestimoDAOList implements EmprestimoDAO{
+
+    private List<Emprestimo> emprestimos = new ArrayList<>();
+    private int novoID = 0;
+
+    public EmprestimoDAOList() {
+        this.emprestimos = emprestimos;
+        this.novoID = novoID;
+    }
+
     @Override
-    public Emprestimo criar(Emprestimo obj) {
-        return null;
+    public List<Emprestimo> getEmprestimos(){
+        return emprestimos;
+    }
+
+    @Override
+    public Emprestimo criar(Emprestimo emprestimo) {
+        emprestimo.setEmprestimoID(this.novoID);
+        this.novoID++;
+        this.emprestimos.add(emprestimo);
+        return emprestimo;
     }
 
     @Override
     public void deletar(int id) {
-
+        for(Emprestimo livro: this.emprestimos){
+            if(livro.getEmprestimoID() == id){
+                this.emprestimos.remove(livro);
+            }
+        }
     }
     @Override
     public void resetar() {
-
+        this.emprestimos = new ArrayList<>();
+        this.novoID = 0;
     }
 
     @Override
@@ -23,9 +48,13 @@ public class EmprestimoDAOList implements EmprestimoDAO{
     }
 
 
-
     @Override
     public Emprestimo procurarPorID(int id) {
+        for (Emprestimo emprestimo : this.emprestimos) {
+            if (emprestimo.getEmprestimoID() == id) {
+                return emprestimo;
+            }
+        }
         return null;
     }
 }
