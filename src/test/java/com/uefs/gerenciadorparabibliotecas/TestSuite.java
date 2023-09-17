b2b58272635f0fe28f825db09178f0c18657eae0
felipe-py <112325208+felipe-py@users.noreply.github.com>
2023-09-17 17:41:38 -0300

    :sparkles: feat: Adicionando suiíe de testes

--- src/test/java/com/uefs/gerenciadorparabibliotecas/TestSuite.java
+++ src/test/java/com/uefs/gerenciadorparabibliotecas/TestSuite.java
@@ -1,5 +1,21 @@
 package com.uefs.gerenciadorparabibliotecas;
 
+import com.uefs.gerenciadorparabibliotecas.dao.emprestimo.EmprestimoDAOListTest;
+import com.uefs.gerenciadorparabibliotecas.dao.funcionario.FuncionarioDAOListTest;
+import com.uefs.gerenciadorparabibliotecas.dao.leitor.LeitorDAOListTest;
+import com.uefs.gerenciadorparabibliotecas.dao.livro.LivroMasterDAOListTest;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+@Suite
+@SuiteDisplayName("Suite de testes")
+@SelectClasses(value = {
+        EmprestimoDAOListTest.class,
+        FuncionarioDAOListTest.class,
+        LeitorDAOListTest.class,
+        LivroMasterDAOListTest.class
+})
 public class TestSuite {
 }
 
