ee19f793590aec9bf4eac1acc6befb6bee308132
felipe-py <112325208+felipe-py@users.noreply.github.com>
2023-09-20 21:10:59 -0300

    :sparkles: feat: Adicionada exceção ao DAO funcionário

--- src/main/java/com/uefs/gerenciadorparabibliotecas/dao/funcionario/FuncionarioDAO.java
+++ src/main/java/com/uefs/gerenciadorparabibliotecas/dao/funcionario/FuncionarioDAO.java
@@ -1,10 +1,11 @@
 package com.uefs.gerenciadorparabibliotecas.dao.funcionario;
 
 import com.uefs.gerenciadorparabibliotecas.dao.CRUD;
+import com.uefs.gerenciadorparabibliotecas.exeptions.funcionarioExceptions.FuncionarioException;
 import com.uefs.gerenciadorparabibliotecas.model.Funcionario;
 
 import java.util.List;
 
-public interface FuncionarioDAO extends CRUD<Funcionario> {
+public interface FuncionarioDAO extends CRUD<Funcionario, FuncionarioException> {
     List<Funcionario> getFuncionarios();
 }
