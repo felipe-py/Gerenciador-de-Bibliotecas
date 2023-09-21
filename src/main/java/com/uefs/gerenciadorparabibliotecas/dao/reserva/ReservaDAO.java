051595f01cf74972817a5986c4997e05a163ca75
felipe-py <112325208+felipe-py@users.noreply.github.com>
2023-09-20 21:11:31 -0300

    :sparkles: feat: Adicionada exceção ao DAO reserva

--- src/main/java/com/uefs/gerenciadorparabibliotecas/dao/reserva/ReservaDAO.java
+++ src/main/java/com/uefs/gerenciadorparabibliotecas/dao/reserva/ReservaDAO.java
@@ -1,11 +1,12 @@
 package com.uefs.gerenciadorparabibliotecas.dao.reserva;
 
 import com.uefs.gerenciadorparabibliotecas.dao.CRUD;
+import com.uefs.gerenciadorparabibliotecas.exeptions.reservaExcetions.ReservaException;
 import com.uefs.gerenciadorparabibliotecas.model.Reserva;
 import org.w3c.dom.stylesheets.LinkStyle;
 
 import java.util.List;
 
-public interface ReservaDAO extends CRUD<Reserva> {
+public interface ReservaDAO extends CRUD<Reserva, ReservaException> {
     List<Reserva> getReservas();
 }
