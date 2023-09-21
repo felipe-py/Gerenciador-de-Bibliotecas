befa08431fc4203cb9d1384c83edb4884bd8242f
felipe-py <112325208+felipe-py@users.noreply.github.com>
2023-09-20 21:10:46 -0300

    :sparkles: feat: Adicionada exceção ao DAO empréstimo

--- src/main/java/com/uefs/gerenciadorparabibliotecas/dao/emprestimo/EmprestimoDAO.java
+++ src/main/java/com/uefs/gerenciadorparabibliotecas/dao/emprestimo/EmprestimoDAO.java
@@ -1,10 +1,11 @@
 package com.uefs.gerenciadorparabibliotecas.dao.emprestimo;
 
 import com.uefs.gerenciadorparabibliotecas.dao.CRUD;
+import com.uefs.gerenciadorparabibliotecas.exeptions.emprestimoExceptions.EmprestimoException;
 import com.uefs.gerenciadorparabibliotecas.model.Emprestimo;
 
 import java.util.List;
 
-public interface EmprestimoDAO extends CRUD<Emprestimo> {
+public interface EmprestimoDAO extends CRUD<Emprestimo, EmprestimoException> {
     List<Emprestimo> getEmprestimos();
 }
