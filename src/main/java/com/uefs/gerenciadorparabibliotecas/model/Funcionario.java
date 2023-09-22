5c038397c9d8daa3b73f9aa4f4e951796bb952e3
felipe-py <112325208+felipe-py@users.noreply.github.com>
2023-09-21 22:26:56 -0300

    :recycle: Exclusão: Retirando atributo nível de privilégio do funcionário

--- src/main/java/com/uefs/gerenciadorparabibliotecas/model/Funcionario.java
+++ src/main/java/com/uefs/gerenciadorparabibliotecas/model/Funcionario.java
@@ -1,18 +1,10 @@
 package com.uefs.gerenciadorparabibliotecas.model;
 
 public abstract class Funcionario extends Pessoa {
-    protected   CategoriaFuncionario    nivelDeProvilegio;
 
     public Funcionario(String novoNome, String novoEndereco, String novaSenha, String novoNumeroDeTelefone, Integer novoUserID) {
         super(novoNome, novoEndereco, novaSenha, novoNumeroDeTelefone, novoUserID);
     }
-    /*public CategoriaFuncionario getNivelDeProvilegio () {
-        return nivelDeProvilegio;
-    }
-
-    public void setNivelDeProvilegio(CategoriaFuncionario nivelDeProvilegio) {
-        this.nivelDeProvilegio = nivelDeProvilegio;
-    }*/
     @Override
     public String toString() {
         return super.toString();
