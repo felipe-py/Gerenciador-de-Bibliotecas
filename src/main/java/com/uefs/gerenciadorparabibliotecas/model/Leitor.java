3ff788d94d09d07964189dfffb899279dd0986f3
felipe-py <112325208+felipe-py@users.noreply.github.com>
2023-09-25 22:00:49 -0300

    :recycle: Refactor: Histórico de empréstimos

--- src/main/java/com/uefs/gerenciadorparabibliotecas/model/Leitor.java
+++ src/main/java/com/uefs/gerenciadorparabibliotecas/model/Leitor.java
@@ -4,24 +4,24 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Leitor extends Pessoa {
-    public int diasDeMulta;
-    private List<Emprestimo> emprestimos;
+    private int diasDeMulta;
+    private List<Emprestimo> historicoEmprestimos;
 
     public void adicionarEmprestimo(Emprestimo emprestimo){
-        this.emprestimos.add(emprestimo);
+        this.historicoEmprestimos.add(emprestimo);
     }
 
     public Leitor(String novoNome, String novoEndereco, String novaSenha, String novoNumeroDeTelefone, int novoUserID) {
         super(novoNome, novoEndereco, novaSenha, novoNumeroDeTelefone, novoUserID);
         this.diasDeMulta = 0;
-        this.emprestimos = new ArrayList<>();
+        this.historicoEmprestimos = new ArrayList<>();
     }
 
     public int getDiasDeMulta() {
         return diasDeMulta;
     }
     public List<Emprestimo> getEmprestimos() {
-        return emprestimos;
+        return historicoEmprestimos;
     }
 
     public void setDiasDeMulta(int diasDeMulta) {
@@ -29,7 +29,7 @@ public class Leitor extends Pessoa {
     }
 
     public void setEmprestimos(List<Emprestimo> emprestimos) {
-        this.emprestimos = emprestimos;
+        this.historicoEmprestimos = emprestimos;
     }
 
 
