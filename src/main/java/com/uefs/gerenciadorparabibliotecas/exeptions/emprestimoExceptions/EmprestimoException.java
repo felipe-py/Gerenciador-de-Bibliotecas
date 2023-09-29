a8b9e00813e52667e8ef8d44b117d8e18510bd7e
felipe-py <112325208+felipe-py@users.noreply.github.com>
2023-09-28 21:46:09 -0300

    :sparkles: feat: Adicionada exceção para renovação do empréstimo

--- src/main/java/com/uefs/gerenciadorparabibliotecas/exeptions/emprestimoExceptions/EmprestimoException.java
+++ src/main/java/com/uefs/gerenciadorparabibliotecas/exeptions/emprestimoExceptions/EmprestimoException.java
@@ -6,6 +6,7 @@ public class EmprestimoException extends Exception{
     public static final String DELETE = "Operação de EXCLUSÃO não realizada.";
     public static final String UPDATE = "Operação de ATUALIZAÇÃO não realizada.";
     public static final String SEARCH = "Operação de BUSCA não realizada.";
+    public static final String RENEW = "Operação de RENOVAÇÃO não realizada";
     private Emprestimo emprestimo;
 
     public EmprestimoException(String create, Emprestimo emprestimo) {
