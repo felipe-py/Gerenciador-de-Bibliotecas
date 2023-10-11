858677d2b12a1a24544a273f5ddc2a7ab87e826e
felipe-py <112325208+felipe-py@users.noreply.github.com>
2023-10-10 22:05:52 -0300

    :sparkles: feat: Adicionado imports a classe Main

--- src/main/java/com/uefs/Main.java
+++ src/main/java/com/uefs/Main.java
@@ -1,5 +1,9 @@
 package com.uefs;
 
+import com.uefs.gerenciadorparabibliotecas.dao.MasterDAO;
+import com.uefs.gerenciadorparabibliotecas.exeptions.leitorExeptions.LeitorException;
+import com.uefs.gerenciadorparabibliotecas.model.Leitor;
+
 /**
  * GERENCIADOR DE BIBLIOTECA
  * @author Luis Felipe Cunha Silva
@@ -7,7 +11,6 @@ package com.uefs;
  * @version 1.0
  */
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws LeitorException {
     }
 }
