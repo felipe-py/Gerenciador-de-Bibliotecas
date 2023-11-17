ddaeac60eb7def940f7da948e690f404250073e9
felipe-py <112325208+felipe-py@users.noreply.github.com>
2023-11-17 19:53:01 -0300

    :red_circle: bug: TestSuite não encontra os testes

--- src/test/java/com/uefs/gerenciadorparabibliotecas/TestSuite.java
+++ src/test/java/com/uefs/gerenciadorparabibliotecas/TestSuite.java
@@ -1,7 +1,7 @@
 package com.uefs.gerenciadorparabibliotecas;
 
 import org.junit.platform.suite.api.*;
-
+//BUG NO TESTSUITE: Nenhum teste é encontrado
 @SelectPackages({
         "com.uefs.gerenciadorparabibliotecas.dao",
         "com.uefs.gerenciadorparabibliotecas.model"})
